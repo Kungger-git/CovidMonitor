a7eb49f6445f3f5e2b1eb076b51377f3f783ec98
KungPaoChick <matthewriel63@gmail.com>
2020-12-09 20:42:21 +0800

    Added Exceptions to catch errors

--- compare_records.py
+++ compare_records.py
@@ -4,24 +4,31 @@ import pandas as pd
 def find_files(file_one, file_two, search_path):
     results = []
     path = os.getcwd() + '/Records/'
-    try:
-        for root, dir, files in os.walk(path + search_path):
-            if file_one + '.csv' and file_two + '.csv' in files:
-                results.append(os.path.join(root, file_one))
-                results.append(os.path.join(root, file_two))
-                dir.append('None')
+    csv_one = file_one + '.csv'
+    csv_two = file_two + '.csv'
+    if os.path.isdir(path + search_path):
+        try:
+            for root, dir, files in os.walk(path + search_path):
+                if csv_one and csv_two in files:
+                    results.append(os.path.join(root, csv_one))
+                    results.append(os.path.join(root, csv_two))
+                    dir.append('None')
+                else:
+                    raise FileNotFoundError('Neither {' + csv_one + ' or ' + csv_two + '} exists in the directories.')
 
-        for result in results:
-            df = pd.read_csv(result)
-            pd.set_option('display.max_rows', None)
-            print(df)
-            print('\n\n')
+            for result in results:
+                df = pd.read_csv(result)
+                pd.set_option('display.max_rows', None)
+                print(df)
+                print('\n\n')
 
-    except FileNotFoundError as io:
-        print('Directory/File has not been found! ', io)
-        
+        except FileNotFoundError as io:
+            print('Directory/File has not been found! ', io)
+    else:
+        raise FileNotFoundError('{' + search_path + '} is not a directory.')
 
-first_file = str(input('First File: '))
-second_file = str(input('Second File: '))
+first_file = str(input('First Country: '))
+second_file = str(input('Second Country: '))
 file_path = str(input('Path:(year/month) '))
+
 find_files(first_file, second_file, file_path)
