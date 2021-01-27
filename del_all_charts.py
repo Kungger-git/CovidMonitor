d311d0733427945d5c11fdb80523b453b8c32974
KungPaoChick <matthewriel63@gmail.com>
2021-01-27 17:26:02 +0800

    Minor changes

--- del_all_charts.py
+++ del_all_charts.py
@@ -1,21 +1,23 @@
-import os
-import shutil
+import os, shutil
 
 
-def del_charts(confirmation):
+def del_charts():
     images, filepath = [], []
     src = os.getcwd() + '/Covid Pie Charts/'
-    if confirmation.casefold() == 'y' or confirmation.casefold() == 'yes':
-        try:
-            for root, dirs, files in os.walk(src):
-                for name in files:
-                    filepath.append(os.path.join(root, name))
-                    if name.endswith(('.png')):
-                        images.append(name)
+    try:
+        for root, dirs, files in os.walk(src):
+            for name in files:
+                filepath.append(os.path.join(root, name))
+                if name.endswith(('.png')):
+                    images.append(name)
+            
+        for path in filepath:
+            print(path + ' {Will be deleted}')
 
+        confirmation = str(
+            input('Are you sure you want to delete all existing records?(y/n) '))
+        if confirmation.casefold() == 'y' or confirmation.casefold() == 'yes':
             shutil.rmtree(src)
-            for path in filepath:
-                print(path + ' {Will be deleted}')
 
             if not os.path.isdir(src):
                 for image in images[0:]:
@@ -25,13 +27,11 @@ def del_charts(confirmation):
                     for name in files:
                         if name.endswith(('.png')):
                             print("{" + name + "} still exists!")
-        except FileNotFoundError as io:
-            print('Directory/File Not Found! ', io)
-    else:
-        print('\nGoodbye!\n')
-        quit()
+        else:
+            print('\nGoodbye!\n')
+            quit()
+    except FileNotFoundError as io:
+        print('Directory/File Not Found! ', io)
+    
 
-
-option = str(
-    input('Are you sure you want to delete all existing records?(y/n) '))
-del_charts(option)
+del_charts()
--- del_all_records.py
+++ del_all_records.py
@@ -1,21 +1,24 @@
 import os, shutil
 
 
-def del_all_records(confirmation):
+def del_all_records():
     filenames, filepath = [], []
-    src = os.getcwd() + '/Records/'
-    if confirmation.casefold() == 'y' or confirmation.casefold() == 'yes':
-        try:
-            for root, dirs, files in os.walk(src):
-                for name in files:
-                    filepath.append(os.path.join(root, name))
-                    if name.endswith(('.csv')):
-                        filenames.append(name)
-
-            shutil.rmtree(src)
-            for path in filepath:
-                print(path + ' {Will be deleted}')
-
+    src = os.getcwd() + '/Records/'    
+    try:
+        for root, dirs, files in os.walk(src):
+            for name in files:
+                filepath.append(os.path.join(root, name))
+                if name.endswith(('.csv')):
+                    filenames.append(name)
+                    
+        for path in filepath:
+            print(path + ' {Will be deleted}')
+    
+        confirmation = str(
+            input('Are you sure you want to delete all existing records?(y/n) '))
+        if confirmation.casefold() == 'y' or confirmation.casefold() == 'yes':
+            shutil.rmtree(src)        
+                    
             if not os.path.isdir(src):
                 for filename in filenames[0:]:
                     print("{" + filename + "} has been deleted successfully!")
@@ -24,13 +27,11 @@ def del_all_records(confirmation):
                     for name in files:
                         if name.endswith(('.csv')):
                             print("{" + name + "} still exists!")
-        except FileNotFoundError as io:
-            print('Directory/File not Found! ', io)
-    else:
-        print('\nGoodbye!\n')
-        quit()
+        else:
+            print('\n\nGoodbye!\n')
+            quit()
+    except FileNotFoundError as io:
+        print('Directory/File not Found! ', io)
 
 
-option = str(
-    input('Are you sure you want to delete all existing records?(y/n) '))
-del_all_records(option)
+del_all_records()
