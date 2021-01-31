6a391d6e5a56494599f697f22b9ce7567ab15d82
KungPaoChick <matthewriel63@gmail.com>
2021-01-31 20:07:20 +0000

    List All country csv in Alphabetical order

--- read_recs.py
+++ read_recs.py
@@ -1,16 +1,21 @@
 from datetime import datetime
-import os
+import os, colorama
 import pandas as pd
 
+
+colorama.init()
 now = datetime.now()
 month, year = now.strftime('%B'), now.strftime('%Y')
-src = os.getcwd() + '/Records/' + year + '/' + month + '/'
+src = os.getcwd() + f'/Records/{year}/{month}/'
 try:
-    for f in os.listdir(src)[0:]:
+    i = 0
+    for f in sorted(os.listdir(src))[0:]:
         if os.path.splitext(f)[1] == '.csv':
+            i+=1
             df = pd.read_csv(src + f)
             pd.set_option('display.max_rows', None)
-            print(df)
-            print('\n\n')
+            print(f'{df}\n\n')
+    print(colorama.Fore.GREEN,
+          f'Total of files: {str(i)}', colorama.Style.RESET_ALL)
 except:
     print("You currently have no records. Execute 'CovidMonitor.py' to create records.")
